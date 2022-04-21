dd74fb40dcf8fe06a37c8ceed04614461042ab93
Jess <jessica.mancoo@hippodigital.co.uk>
2022-04-21 14:06:46 +0100

    index and start update
    index and start update

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Ride or Die',
+  serviceName: 'Ride or die',
 
   // Default port that prototype runs on
   port: '3000',
